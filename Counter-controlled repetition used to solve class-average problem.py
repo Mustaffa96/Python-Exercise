cbaab5de85ae16ff594dcf2356cc01a0ed8fb96f
Ahmad Mustaffa <49057200+Mustaffa96@users.noreply.github.com>
2021-08-11 23:06:07 +0800

    Add files via upload

---
+++ Counter-controlled repetition used to solve class-average problem.py
@@ -0,0 +1,12 @@
+total = 0 # sum of grades
+gradeCounter = 1 # number of grades entered
+
+# processing phase
+while gradeCounter <= 10: # loop 10 times
+    grade = input( "Enter grade: " ) # get one grade
+    grade = int( grade ) # convert string to an integer
+    total = total + grade
+    gradeCounter = gradeCounter + 1
+
+average = total / 10 # integer division
+print ("Class average is" , average)
